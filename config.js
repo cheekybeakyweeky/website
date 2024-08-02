7ec8d136ef8e61bf920de71823c72dbd5a0f2fe0
cheekybeakyweeky <135086945+cheekybeakyweeky@users.noreply.github.com>
2024-08-02 15:05:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,14 @@ const config = {
   portfolioTitle: "Eswancoders Portfolio",
   aboutMe: {
     heading: "About Me",
-    description: `Hello! I am a scripter. I have been programming for 6 years. I have been learning LUA for the past 2 years.  Thank you!`
+    description: `Hello! I am a Roblox scripter. I have been programming for 6 years. I have been learning LUA for the past 2 years. I love seeing how things work and am always open to crazy ideas. Below is some of my past work, it is old and I am updating it whenever I have time. Thank you for thinking about choosing me!`
   },
   pastWork: {
     heading: "Past Work",
     videos: [
       { src: "v1.mp4", type: "video/mp4" },
       { src: "v2.mp4", type: "video/mp4" },
+      { src: "v3.mp4", type: "video/mp4" },
       { src: "v4.mp4", type: "video/mp4" },
       { src: "v5.mp4", type: "video/mp4" }
     ]
@@ -17,10 +18,13 @@ const config = {
   pricing: {
     heading: "Pricing",
     details: [
-     
+      { description: "Small commissions: Simple mechanics/game systems like tools, round system, datastores, etc.", price: "200-900" },
+      { description: "Medium commissions: More complex version of small commissions. Larger games, mechanics, etc.", price: "900-1800" },
+      { description: "Large commissions: Whole games, complex mechanics, anything everyone knows it's hard to do.", price: "1800-3000" }
     ]
   },
   contacts: [
-   
+    { href: "https://www.roblox.com/users/2713185027/profile", imgSrc: "https://i.imgur.com/sKtxGz4.png", alt: "Roblox profile" },
+    { href: "https://discord.com/channels/@me", imgSrc: "https://i.imgur.com/JOyLkB0.png", alt: "discord: eswancoder" }
   ]
 };
